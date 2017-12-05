6b0ea616f86828225f4050dceb1c0d6b3d8af724
adsf <runaround340@gmail.com>
2017-12-05 20:05:49 +0000

    Changes to mithraeum

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,17 @@ public class Player : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical"); //allows w/ arrow or WASD
         float moveHorizontal = Input.GetAxis("Horizontal");
 
+        //change my x and z values based on camera position 
+        //get x input from camera
+        //get y input from camera
+
+        /*
+        Vector3 playerRotate = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+
+        Quaternion direction = Quaternion.LookRotation(playerRotate);
+        transform.rotation = Quaternion.Lerp(transform.rotation, direction, Time.deltaTime);
+    
+    */
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(-movement * speed);
 
