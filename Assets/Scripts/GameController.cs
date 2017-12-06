65e6592cd26ad0548a37bb40ec828b442e43eb80
adsf <runaround340@gmail.com>
2017-12-06 19:51:11 +0000

    Added entire fort

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	void Update ()
+    {
+        //if at anytime the user pushes Escape, go to main Menu
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,6 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
     }
 
     void FixedUpdate()
@@ -18,17 +17,10 @@ public class Player : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical"); //allows w/ arrow or WASD
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //change my x and z values based on camera position 
+        //change my x and z values based on camera position --get info from MouseLook Script
         //get x input from camera
         //get y input from camera
 
-        /*
-        Vector3 playerRotate = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
-
-        Quaternion direction = Quaternion.LookRotation(playerRotate);
-        transform.rotation = Quaternion.Lerp(transform.rotation, direction, Time.deltaTime);
-    
-    */
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(-movement * speed);
 
