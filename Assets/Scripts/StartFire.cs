12d9ff87d467eaeb7519014eaeb430334da71641
Deb <dmayers340@gmail.com>
2018-03-23 16:56:44 +0000

    Working on VR

---
+++ Assets/Scripts/StartFire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFire : MonoBehaviour {
+
+    public GameObject torch;
+    public ParticleSystem fire;
+
+    void Start()
+    {
+        fire = fire.GetComponent<ParticleSystem>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == ("torch"))
+        {
+            fire.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/TorchLighting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLighting : MonoBehaviour
+{
+    public ParticleSystem fire;
+    public List<ParticleCollisionEvent> lightingEvents;
+
+    // Use this for initialization
+    void Start ()
+    {
+        fire = GetComponent<ParticleSystem>();
+        lightingEvents = new List<ParticleCollisionEvent>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+		
+	}
+}
