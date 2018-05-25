da09e8c6154b66762037aeee8ab983271a44e24f
Deb <dmayers340@gmail.com>
2018-05-25 15:05:16 +0100

    changed lighting

--- Assets/Scripts/PlayerCollideWithTorch.cs
+++ Assets/Scripts/PlayerCollideWithTorch.cs
@@ -2,29 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Lighting torch script. 
+ * Reqs: 
+ * 1.) Attach to player
+ * 2.) The particle system on torch to be lit should be tagged as 'torch'
+ * 3.) Also need to have a collider on bit that has particle system as trigger
+ * Created with help: https://stackoverflow.com/questions/49733313/unity3d-having-player-light-torches-using-particlesystem-and-oncollisionenter
+ * */
+
 public class PlayerCollideWithTorch : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("torch")) //if player collides with torch
+        //If player collides with torch
+        if (other.CompareTag("torch")) 
         {
             //Get particle system
             ParticleSystem ps = other.GetComponent<ParticleSystem>();
 
-            //play particle system
+            //Play particle system
             ps.Play();
         }
     }
-
-    /*
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("torch"))
-        {
-            ParticleSystem ps = other.GetComponent<ParticleSystem>();
-
-            ps.Stop();
-        }
-    }
-    */
 }
