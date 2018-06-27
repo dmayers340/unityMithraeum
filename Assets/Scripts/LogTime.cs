90b2fd6de8194120776745e245ee9c4f70910efc
Deb <dmayers340@gmail.com>
2018-06-27 12:00:47 +0100

    Changes to the environment

--- Assets/Scripts/LogTime.cs
+++ Assets/Scripts/LogTime.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogTime : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Button sceneButton;
+    public Text thisText;
+    public int sceneIndex;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(GameObject.Find("canvas"));
+
+        if(sceneButton != null)
+        {
+            sceneButton.onClick.AddListener(LoadSceneButton);
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        thisText.text = "Time Since Level Was Loaded: " + Time.timeSinceLevelLoad;
 	}
+
+    void LoadSceneButton()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
 }
---
+++ Assets/Scripts/MenuSelect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuSelect : MonoBehaviour
+{ 
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject menu;
+
+    void OnEnable()
+    {
+       // controllerEvents.ButtonOnePressed += ControllerEvents_ButonOnePressed;
+        //controllerEvents.ButtonOneReleased += ControllerEvents_ButonOneReleased;
+
+    }
+}
