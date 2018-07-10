503baf0197027e3b764611a6198e3cfa31ca83e8
Deb <dmayers340@gmail.com>
2018-07-10 11:06:13 +0100

    Changed temple lighting

--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -10,34 +10,26 @@ using VRTK;
  * */
 public class GameManagement : MonoBehaviour
 {
-    /*
     LogTime timeScript;
     Distance distanceScript;
     NumberGrabbedObjects grabbedScript;
-    VRTK_InteractableObject countObject;
-
-	// Use this for initialization
+    public GameObject gameManagement;
     /*
-	void Start ()
+    void Start()
     {
-        timeScript = this.AddComponent(Type.GetType("LogTime"));
-        distanceScript = this.AddComponenet(Type.GetType("Distance"));
-        grabbedScript = this.AddComponent<NumberGrabbedObjects>();
-        countObject = this.AddComponent<VRTK_InteractableObject>();
+        gameManagement = GameObject.Find("management");
+
+        timeScript = gameManagement.AddComponent<LogTime>(); 
+        distanceScript = gameManagement.AddComponent<Distance>();
+        grabbedScript = gameManagement.AddComponent<NumberGrabbedObjects>();
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 
     void OnApplicationQuit()
     {
         Debug.Log("Count of Grabbed Objects " + grabbedScript.getGrabbedObjects());
         Debug.Log("Application Distance Walked: " + distanceScript.getDistanceWalked());
         Debug.Log("Time Spent: " + timeScript.getTimeSpent());
-        Debug.Log("VRTK count object " + countObject.getCountOfObjectsGrabbed());
     }
     */
+   
 }
