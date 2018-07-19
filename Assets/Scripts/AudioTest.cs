2b74a043533ed30293e8d1afaee6a0e3349472f2
Deb <dmayers340@gmail.com>
2018-07-19 12:51:49 +0100

    Still working on Audio Queue. Adding the clips multiple time (on enter) but still overplaying

--- Assets/Scripts/AudioTest.cs
+++ Assets/Scripts/AudioTest.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class AudioTest : MonoBehaviour
 {
-    [SerializeField] AudioSource audioSource;
+    AudioSource audioSource;
+    Queue<AudioSource> queuAudio = new Queue<AudioSource>();
+
     private Queue<AudioClip> queueClips = new Queue<AudioClip>();
 
+    AudioClip clip;
+    bool hasEnteredZone = false;
+
+    public void AddAudioSource(AudioSource source)
+    {
+        bool hasBeenAdded = false;
+
+       queuAudio.Enqueue(source);
+
+        if(queuAudio.Count == 1 && !source.isPlaying && hasBeenAdded == false)
+        {
+            PlayAudio();
+            hasBeenAdded = true;
+        }
+        Debug.Log("Added Audio Source to Queue: " + queuAudio.Count);
+        
+    }
+
+    //Add the audio clip to the queue
     public void Add(AudioClip clip)
     {
         if (clip != null)
@@ -17,34 +38,36 @@ public class AudioTest : MonoBehaviour
         if (queueClips.Count == 1)
         {
             PlayAudio();
+            Debug.Log("Audio is being Played");
         }
     }
 
+    //Play the audio by getting the clip from the queue
     public void PlayAudio()
     {
-        while (queueClips.Count != 0)
-        {
-            AudioClip clipToPlay = GetAudioClip();
-            audioSource.clip = clipToPlay;
-            // audioSource.PlayOneShot(clipToPlay);
-            audioSource.Play();
-        }
+       // AudioClip clip = queueClips.Dequeue();
+        //audioSource.clip = clip;
+
+        AudioSource toPlay = queuAudio.Dequeue();
+       if (!toPlay.isPlaying)
+       {
+            toPlay.Play();   
+       }
     }
 
+    //When the player enters, get the audio source
+    //Then get the clip from that source and put in the queue
+    //Then Play the audio from the queue
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("zone"))
         {
-            AudioSource toAdd = other.GetComponent<AudioSource>();
-            AudioClip clipToAdd = toAdd.clip;
-            Add(clipToAdd);
+            AudioSource zoneSource = other.GetComponent<AudioSource>();
+            AddAudioSource(zoneSource);
+           // AudioSource toAdd = other.GetComponent<AudioSource>();
+            //AudioClip clipToAdd = toAdd.clip;
+          //  Add(clipToAdd);
         }
-    }
-
-    public AudioClip GetAudioClip()
-    {
-        AudioClip current = queueClips.Dequeue();
-        return current;
     }
 }
 
